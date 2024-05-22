1dc5f42d38f7130fe5848af254205e305d899295
Mirza Kadric <mirza.kadric@authoritypartners.com>
2024-05-22 20:01:57 +0200

    appointments service communication

--- StressReliefManagement/src/main/java/psychotherapy/stressrelief/StressReliefApplication.java
+++ StressReliefManagement/src/main/java/psychotherapy/stressrelief/StressReliefApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import psychotherapy.stressrelief.datacontext.model.BreathControl;
 import psychotherapy.stressrelief.datacontext.model.StressReliefAction;
 import psychotherapy.stressrelief.datacontext.repository.StressReliefActionRepository;
@@ -13,6 +14,7 @@ import java.time.ZoneOffset;
 import java.util.UUID;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class StressReliefApplication implements CommandLineRunner {
 
 	@Autowired
--- StressReliefManagement/src/main/java/psychotherapy/stressrelief/features/meditation/createmeditationlog/CreateMeditationLogHandler.java
+++ StressReliefManagement/src/main/java/psychotherapy/stressrelief/features/meditation/createmeditationlog/CreateMeditationLogHandler.java
@@ -1,11 +1,13 @@
 package psychotherapy.stressrelief.features.meditation.createmeditationlog;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import psychotherapy.stressrelief.datacontext.model.Meditation;
 import psychotherapy.stressrelief.datacontext.model.StressReliefAction;
 import psychotherapy.stressrelief.datacontext.repository.StressReliefActionRepository;
+import psychotherapy.stressrelief.features.patient.checkpatientexistence.CheckPatientExistenceHandler;
 
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
@@ -14,11 +16,18 @@ import java.time.ZoneOffset;
 public class CreateMeditationLogHandler {
     @Autowired
     private StressReliefActionRepository stressReliefActionRepository;
+    @Autowired
+    private CheckPatientExistenceHandler patientExistenceHandler;
+
     public ResponseEntity<CreateMeditationLogResponse> handle(CreateMeditationLogRequest request) {
+        var patientExists = patientExistenceHandler.handle(request.getPatientId());
+        if (!patientExists) {
+            return ResponseEntity.notFound().build();
+        }
         StressReliefAction action = CreateMeditationLogMapper.INSTANCE.requestToEntity(request);
         action.setStartedAt(OffsetDateTime.now().withOffsetSameInstant(ZoneOffset.UTC));
         Meditation savedMeditation = (Meditation) stressReliefActionRepository.save(action);
         CreateMeditationLogResponse response = CreateMeditationLogMapper.INSTANCE.entityToResponse(savedMeditation);
-        return ResponseEntity.ok(response);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 }
