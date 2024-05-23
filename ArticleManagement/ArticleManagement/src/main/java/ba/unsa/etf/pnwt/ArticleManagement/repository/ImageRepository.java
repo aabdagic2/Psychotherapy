1346c4d2b8f840ccbf9251825c6ac872039b4803
Anida Nezovic <anezovic1@etf.unsa.ba>
2024-05-23 12:48:37 +0200

    GRPC implemented in ArticleManagement

--- ArticleManagement/ArticleManagement/src/main/java/ba/unsa/etf/pnwt/ArticleManagement/repository/ImageRepository.java
+++ ArticleManagement/ArticleManagement/src/main/java/ba/unsa/etf/pnwt/ArticleManagement/repository/ImageRepository.java
@@ -2,6 +2,8 @@ package ba.unsa.etf.pnwt.ArticleManagement.repository;
 
 import ba.unsa.etf.pnwt.ArticleManagement.model.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ImageRepository extends JpaRepository<Image, String> {
 }
--- ArticleManagement/ArticleManagement/src/main/java/ba/unsa/etf/pnwt/ArticleManagement/repository/VideoRepository.java
+++ ArticleManagement/ArticleManagement/src/main/java/ba/unsa/etf/pnwt/ArticleManagement/repository/VideoRepository.java
@@ -2,6 +2,8 @@ package ba.unsa.etf.pnwt.ArticleManagement.repository;
 
 import ba.unsa.etf.pnwt.ArticleManagement.model.Video;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface VideoRepository extends JpaRepository<Video, String> {
 }
