bce03e48607566ea53ecda843dcbe89c0db6c66e
Mirza Kadric <mirza.kadric@authoritypartners.com>
2024-06-03 15:11:15 +0200

    add grpc for stress relief service

--- StressReliefManagement/src/main/java/psychotherapy/stressrelief/features/meditation/createmeditationlog/CreateMeditationLogHandler.java
+++ StressReliefManagement/src/main/java/psychotherapy/stressrelief/features/meditation/createmeditationlog/CreateMeditationLogHandler.java
@@ -1,5 +1,7 @@
 package psychotherapy.stressrelief.features.meditation.createmeditationlog;
 
+import ba.unsa.etf.pnwt.proto.LoggingRequest;
+import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +20,8 @@ public class CreateMeditationLogHandler {
     private StressReliefActionRepository stressReliefActionRepository;
     @Autowired
     private CheckPatientExistenceHandler patientExistenceHandler;
+    @GrpcClient("grpc")
+    ba.unsa.etf.pnwt.proto.LoggingServiceGrpc.LoggingServiceBlockingStub loggingServiceBlockingStub;
 
     public ResponseEntity<CreateMeditationLogResponse> handle(CreateMeditationLogRequest request) {
         var patientExists = patientExistenceHandler.handle(request.getPatientId());
@@ -27,6 +31,14 @@ public class CreateMeditationLogHandler {
         StressReliefAction action = CreateMeditationLogMapper.INSTANCE.requestToEntity(request);
         action.setStartedAt(OffsetDateTime.now().withOffsetSameInstant(ZoneOffset.UTC));
         Meditation savedMeditation = (Meditation) stressReliefActionRepository.save(action);
+        LoggingRequest loggingRequest = LoggingRequest.newBuilder()
+                .setServiceName("StressReliefService")
+                .setControllerName("StressReliefActionController")
+                .setActionUrl("/stressrelief/meditation")
+                .setActionType("POST")
+                .setActionResponse("SUCCESS")
+                .build();
+        loggingServiceBlockingStub.logRequest(loggingRequest);
         CreateMeditationLogResponse response = CreateMeditationLogMapper.INSTANCE.entityToResponse(savedMeditation);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
