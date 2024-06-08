5848efa5806ca48a1ce2147c9db071a11098936b
zlata <zohran1@etf.unsa.ba>
2024-06-08 14:34:45 +0200

    proba

---
+++ Eureka/src/test/java/com/psychotherapy/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psychotherapy.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
